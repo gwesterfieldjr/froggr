35ab35c193f82f885f3d53c0147c96a1e990dceb
rramsaro <rramsaro@ggc.edu>
2013-02-03 14:44:59 -0500

    Added canvas and made program runnable

---
+++ src/game/FroggrGameFrame.java
@@ -0,0 +1,19 @@
+package game;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+public class FroggrGameFrame {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Froggr");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FroggrGame game = new FroggrGame();
+		frame.setSize(game.WIDTH, game.HEIGHT);
+		frame.setLayout(new BorderLayout());
+		frame.add(game, BorderLayout.CENTER);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		game.start();
+	}
+}
