1d52771dd848f913de428870e98848f958486447
rramsaro <rramsaro@ggc.edu>
2013-02-25 02:00:36 -0500

    res folder added to src folder. Changed the way images are loaded in application class.

--- src/game/FroggrGameApplication.java
+++ src/game/FroggrGameApplication.java
@@ -20,21 +20,21 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import sprites.Player;
+import sprites.Sprite;
 
 /**
  * 
- * @author Raj Ramsaroop
- * 		   Greg Westerfield, Jr.
+ * @author Raj Ramsaroop Greg Westerfield, Jr.
  * 
  */
 public class FroggrGameApplication implements ActionListener {
 
 	// Froggr game driver class
 	private FroggrGame game;
-	
+
 	// Frame
 	private static JFrame frame;
-	
+
 	// Application Panels
 	public static final JPanel pnlTitleScreen = new JPanel();
 	public static final JPanel pnlGameScreen = new JPanel();
@@ -49,10 +49,10 @@ public class FroggrGameApplication implements ActionListener {
 	// Label graphics
 	private JLabel lblTitleScreenGraphic;
 	private JLabel lblArrowKeysGraphic;
-	
+
 	// Instructions Screen Header
 	private JLabel lblInstructionsHeader;
-	
+
 	// Instructions Screen TextArea
 	private JTextArea txtAreaInstructions;
 
@@ -88,12 +88,13 @@ public class FroggrGameApplication implements ActionListener {
 	 */
 	private void setFrameIcon() {
 		try {
-			BufferedImage image = ImageIO.read(Player.class.getClassLoader().getResource( 
-					"res/sprites/player/player-idle.gif"));
+			BufferedImage image = ImageIO.read(Player.class.getClassLoader()
+					.getResource("res/sprites/player/player-idle.gif"));
 			frame.setIconImage(image);
 		} catch (IOException e) {
-			System.out.println("ERROR: Could not load idle player sprite image.");
-		} 		
+			System.out
+					.println("ERROR: Could not load idle player sprite image.");
+		}
 	}
 
 	/**
@@ -103,12 +104,13 @@ public class FroggrGameApplication implements ActionListener {
 	private void createGameScreen() {
 		// Set Panel information
 		pnlGameScreen.setLayout(null);
-		pnlGameScreen.setPreferredSize(new Dimension(FroggrGame.GAME_WIDTH + 9, FroggrGame.GAME_HEIGHT + 30));
+		pnlGameScreen.setPreferredSize(new Dimension(FroggrGame.GAME_WIDTH + 9,
+				FroggrGame.GAME_HEIGHT + 30));
 		pnlGameScreen.setBackground(BACKGROUND_COLOR);
-		
+
 		// Initialize the Froggr Game Driver Class
 		game = new FroggrGame();
-		
+
 		// Add Game component to the panel
 		pnlGameScreen.add(game);
 	}
@@ -119,7 +121,8 @@ public class FroggrGameApplication implements ActionListener {
 	private void createInstructionScreen() {
 		// Set panel information
 		pnlInstructionsScreen.setLayout(null);
-		pnlInstructionsScreen.setPreferredSize(new Dimension(FroggrGame.GAME_WIDTH + 9, FroggrGame.GAME_HEIGHT + 30));
+		pnlInstructionsScreen.setPreferredSize(new Dimension(
+				FroggrGame.GAME_WIDTH + 9, FroggrGame.GAME_HEIGHT + 30));
 		pnlInstructionsScreen.setBackground(BACKGROUND_COLOR);
 
 		// Set Instructions Header label
@@ -127,43 +130,47 @@ public class FroggrGameApplication implements ActionListener {
 		lblInstructionsHeader.setFont(new Font("Arial", Font.BOLD, 50));
 		lblInstructionsHeader.setForeground(Color.RED);
 		lblInstructionsHeader.setBounds(0, 25, 500, 50);
-		
+
 		// Set Instructions label
-		txtAreaInstructions = new JTextArea("     The player starts with three frogs (lives). The player’s goal is to guide the frog from\n" +
-				" the bottom of the screen into one of the four winning zones where flies wait to be eaten\n" +
-				" as lunch.\n\n" + 
-				"     The lower half of the screen contains a road with motor vehicles, which include\n" +
-				" different sized cars and trucks speeding along it horizontally. The middle of the screen,\n" +
-				" after the road, contains a median where the player must prepare to navigate the river.\n" +
-				" The upper half of the screen consists of a river with logs, turtles, and lilies all moving\n" +
-				" horizontally across the screen. The very top of the screen contains four frog homes\n" +
-				" which are the destinations for each frog. To beat the game the player must fill each of\n" +
-				" the four frog homes with a frog. The player has three lives to do this.\n\n" +
-				"     There are many different ways to lose a life in this game, including:\n\n " +
-				"        1) Being hit by a road vehicle.\n" +
-				"         2) Jumping into the river's water.\n" +
-				"         3) Staying on top of a platform as it goes of the screen.\n" +
-				"         4) Jumping into a home already occupied by a frog.\n" +
-				"         5) Jumping into the side of a frog home.\n\n" +
-				"   LEFT ARROW  ::  Move left\n" +
-				"   RIGHT ARROM  :: Move Right\n" +
-				"   UP ARROW  ::  Move forward\n" +
-				"   DOWN ARROW  ::  Move backward\n" +
-				"   ESC  ::  Pause the game");
+		txtAreaInstructions = new JTextArea(
+				"     The player starts with three frogs (lives). The player’s goal is to guide the frog from\n"
+						+ " the bottom of the screen into one of the four winning zones where flies wait to be eaten\n"
+						+ " as lunch.\n\n"
+						+ "     The lower half of the screen contains a road with motor vehicles, which include\n"
+						+ " different sized cars and trucks speeding along it horizontally. The middle of the screen,\n"
+						+ " after the road, contains a median where the player must prepare to navigate the river.\n"
+						+ " The upper half of the screen consists of a river with logs, turtles, and lilies all moving\n"
+						+ " horizontally across the screen. The very top of the screen contains four frog homes\n"
+						+ " which are the destinations for each frog. To beat the game the player must fill each of\n"
+						+ " the four frog homes with a frog. The player has three lives to do this.\n\n"
+						+ "     There are many different ways to lose a life in this game, including:\n\n "
+						+ "        1) Being hit by a road vehicle.\n"
+						+ "         2) Jumping into the river's water.\n"
+						+ "         3) Staying on top of a platform as it goes of the screen.\n"
+						+ "         4) Jumping into a home already occupied by a frog.\n"
+						+ "         5) Jumping into the side of a frog home.\n\n"
+						+ "   LEFT ARROW  ::  Move left\n"
+						+ "   RIGHT ARROM  :: Move Right\n"
+						+ "   UP ARROW  ::  Move forward\n"
+						+ "   DOWN ARROW  ::  Move backward\n"
+						+ "   ESC  ::  Pause the game");
 		txtAreaInstructions.setFont(new Font("Arial", Font.BOLD, 12));
 		txtAreaInstructions.setBackground(Color.BLACK);
 		txtAreaInstructions.setForeground(Color.WHITE);
 		txtAreaInstructions.setBounds(0, 85, 500, 375);
-		
+
 		// Set Arrow Keys Graphic
-		lblArrowKeysGraphic = new JLabel(new ImageIcon("res/images/arrowkeys.png"));
+		lblArrowKeysGraphic = new JLabel(new ImageIcon(
+				"res/images/arrowkeys.png"));
 		lblArrowKeysGraphic.setBorder(BorderFactory.createEmptyBorder());
 		lblArrowKeysGraphic.setBounds(75, 425, 356, 287);
-		
+
 		// Set the back to the title screen button
-		btnBackToTitleScreen = new JButton(new ImageIcon("res/images/menu/back.png")); // Temporary images
+		btnBackToTitleScreen = new JButton(new ImageIcon(
+				"res/images/menu/back.png")); // Temporary images
 		btnBackToTitleScreen.setRolloverEnabled(true);
-		btnBackToTitleScreen.setRolloverIcon(new ImageIcon("res/images/menu/back-hover.png"));
+		btnBackToTitleScreen.setRolloverIcon(new ImageIcon(
+				"res/images/menu/back-hover.png"));
 		btnBackToTitleScreen.addActionListener(this);
 		btnBackToTitleScreen.setBorder(BorderFactory.createEmptyBorder());
 		btnBackToTitleScreen.setContentAreaFilled(false);
@@ -182,40 +189,84 @@ public class FroggrGameApplication implements ActionListener {
 	private void createTitleScreen() {
 		// Set panel information
 		pnlTitleScreen.setLayout(null);
-		pnlTitleScreen.setPreferredSize(new Dimension(FroggrGame.GAME_WIDTH + 9, FroggrGame.GAME_HEIGHT + 30));
+		pnlTitleScreen.setPreferredSize(new Dimension(
+				FroggrGame.GAME_WIDTH + 9, FroggrGame.GAME_HEIGHT + 30));
 		pnlTitleScreen.setBackground(BACKGROUND_COLOR);
 
 		// Set Title Screen Graphic
-		lblTitleScreenGraphic = new JLabel(new ImageIcon("res/images/title-screen.png"));
+		try {
+			lblTitleScreenGraphic = new JLabel(new ImageIcon(
+					ImageIO.read(Sprite.class.getClassLoader().getResource(
+							"res/images/title-screen.png"))));
+		} catch (IOException e) {
+			System.out.println("Could not load title screen image!");
+		}
 		lblTitleScreenGraphic.setBorder(BorderFactory.createEmptyBorder());
 		lblTitleScreenGraphic.setBounds(0, 75, 500, 307);
 
 		// Set Start Game Button
-		btnStartGame = new JButton(new ImageIcon("res/images/menu/start.png")); // Temporary images
+		try {
+			btnStartGame = new JButton(
+					new ImageIcon(ImageIO.read(Sprite.class.getClassLoader()
+							.getResource("res/images/menu/start.png"))));
+		} catch (IOException e) {
+			System.out.println("Could not load start button image!");
+		} // Temporary images
 		btnStartGame.setRolloverEnabled(true);
-		btnStartGame.setRolloverIcon(new ImageIcon("res/images/menu/start-hover.png"));
+		try {
+			btnStartGame.setRolloverIcon(new ImageIcon(ImageIO
+					.read(Sprite.class.getClassLoader().getResource(
+							"res/images/menu/start-hover.png"))));
+		} catch (IOException e) {
+			System.out.println("Could not load start button hover image!");
+		}
 		btnStartGame.addActionListener(this);
 		btnStartGame.setBorder(BorderFactory.createEmptyBorder());
 		btnStartGame.setContentAreaFilled(false);
 		btnStartGame.setBounds(150, 400, 200, 50);
 
 		// Set Directions Button
-		btnInstructions = new JButton(new ImageIcon("res/images/menu/instructions.png"));
+		try {
+			btnInstructions = new JButton(new ImageIcon(
+					ImageIO.read(Sprite.class.getClassLoader().getResource(
+							"res/images/menu/instructions.png"))));
+		} catch (IOException e) {
+			System.out.println("Could not load instructions button image!");
+		}
 		btnInstructions.setRolloverEnabled(true);
-		btnInstructions.setRolloverIcon(new ImageIcon("res/images/menu/instructions-hover.png"));
+		try {
+			btnInstructions.setRolloverIcon(new ImageIcon(ImageIO
+					.read(Sprite.class.getClassLoader().getResource(
+							"res/images/menu/instructions-hover.png"))));
+		} catch (IOException e) {
+			System.out
+					.println("Could not load instructions button hover image!");
+		}
 		btnInstructions.addActionListener(this);
 		btnInstructions.setBorder(BorderFactory.createEmptyBorder());
 		btnInstructions.setContentAreaFilled(false);
 		btnInstructions.setBounds(150, 450, 200, 50);
-		
+
 		// Set Credits Button
-		btnCredits = new JButton(new ImageIcon("res/images/menu/credits.png"));
+		try {
+			btnCredits = new JButton(new ImageIcon(ImageIO.read(Sprite.class
+					.getClassLoader()
+					.getResource("res/images/menu/credits.png"))));
+		} catch (IOException e) {
+			System.out.println("Could not load credits button image!");
+		}
 		btnCredits.setRolloverEnabled(true);
-		btnCredits.setRolloverIcon(new ImageIcon("res/images/menu/credits-hover.png"));
+		try {
+			btnCredits.setRolloverIcon(new ImageIcon(ImageIO.read(Sprite.class
+					.getClassLoader().getResource(
+							"res/images/menu/credits-hover.png"))));
+		} catch (IOException e) {
+			System.out.println("Could not load credits button hover image!");
+		}
 		btnCredits.addActionListener(this);
 		btnCredits.setBorder(BorderFactory.createEmptyBorder());
 		btnCredits.setContentAreaFilled(false);
-		btnCredits.setBounds(150,500, 200, 50);
+		btnCredits.setBounds(150, 500, 200, 50);
 
 		// Add components to the panel
 		pnlTitleScreen.add(lblTitleScreenGraphic);
@@ -248,10 +299,13 @@ public class FroggrGameApplication implements ActionListener {
 	 * Launches a pop up that displays the games version and authors.
 	 */
 	private void launchCredits() {
-		JOptionPane.showMessageDialog(null,"Version:  0.1\n\nAuthors:  Raj Ramsaroop (Programmer)\n" +
-				"                  Greg Westerfield, Jr. (Programmer)\n                  James Fox (Graphics)",
-				"Credits", 1);
-		}
+		JOptionPane
+				.showMessageDialog(
+						null,
+						"Version:  0.1\n\nAuthors:  Raj Ramsaroop (Programmer)\n"
+								+ "                  Greg Westerfield, Jr. (Programmer)\n                  James Fox (Graphics)",
+						"Credits", 1);
+	}
 
 	/**
 	 * Displays the title screen
@@ -281,7 +335,7 @@ public class FroggrGameApplication implements ActionListener {
 		showGameScreen();
 		game.start();
 	}
-	
+
 	public static void quitGame() {
 		frame.dispose();
 	}
