c572fe188ef4078372143479f23503e1b623abb4
rramsaro <rramsaro@ggc.edu>
2013-02-25 02:25:58 -0500

    Changed the way audio files are loaded.

--- src/util/SoundEffect.java
+++ src/util/SoundEffect.java
@@ -1,57 +1,60 @@
 package util;
 
 import java.io.File;
+import java.net.URL;
 
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 /**
  * 
- * @author Raj Ramsaroop 
- *         Greg Westerfield, Jr.
- *
+ * @author Raj Ramsaroop Greg Westerfield, Jr.
+ * 
  */
 public class SoundEffect {
+
 	
+
 	/**
 	 * Sound for player movement
 	 */
-	public static final File MOVE = new File("res/sounds/player-movement.wav");
-	
+	public static final URL MOVE = SoundEffect.class.getClassLoader().getResource("res/sounds/player-movement.wav");
+
 	/**
 	 * Sound for sprite collision
 	 */
-	public static final File COLLISION = new File("res/sounds/sprite-collision.wav");
-	
+	public static final URL COLLISION = SoundEffect.class.getClassLoader().getResource(
+			"res/sounds/sprite-collision.wav");
+
 	/**
 	 * Sound for sprite splash
 	 */
-	public static final File SPLASH = new File("res/sounds/splash.wav");
-	
+	public static final URL SPLASH = SoundEffect.class.getClassLoader().getResource("res/sounds/splash.wav");
+
 	/**
 	 * Sound for victory
 	 */
-	public static final File VICTORY = new File("res/sounds/victory.wav");
-	
+	public static final URL VICTORY = SoundEffect.class.getClassLoader().getResource("res/sounds/victory.wav");
+
 	/**
 	 * Plays the sound effect
-	 * @param fileName of sound
+	 * 
+	 * @param fileName
+	 *            of sound
 	 */
-	public static void play(File fileName)
-	{
-	    try
-	    {
-	        Clip clip = AudioSystem.getClip();
-	        clip.open(AudioSystem.getAudioInputStream(fileName));
-	        if (clip.isRunning()){
-	        	clip.stop();
-	        	clip.loop(0);
-	        }
-	        clip.start();
-	    }
-	    catch (Exception exc)
-	    {
-	        exc.printStackTrace(System.out);
-	    }
+	public static void play(URL fileName) {
+		try {
+			AudioInputStream audioIn = AudioSystem.getAudioInputStream(fileName);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioIn);
+			if (clip.isRunning()) {
+				clip.stop();
+				clip.loop(0);
+			}
+			clip.start();
+		} catch (Exception exc) {
+			exc.printStackTrace(System.out);
+		}
 	}
 }
